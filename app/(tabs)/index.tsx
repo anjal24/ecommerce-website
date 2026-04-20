e049a0994f8eafcaa677fcbac03b7469c6081f05
anjal24 <anjalmishraa1@gmail.com>
2026-04-20 23:30:38 +0530

    updated app changes

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -51,6 +51,8 @@ onScroll={(e)=>{
     <View key={index} className='relative w-full h-48 bg-gray-200 overflow-hidden' style={{width:width-32}}>
 <Image source={{uri:banner.image}}
 className='w-full h-full' resizeMode='cover'/>
+<View className='absolute inset-0 bg-black/40'>  // semi transparent overlay
+
 <View className='absolute bottom-4 left-4 z-10'>
   <Text className='text-white text-2xl font-bold'>{banner.title}</Text>
   <Text className='text-white text-sm font-medium'>{banner.subtitle}</Text>
@@ -58,7 +60,7 @@ className='w-full h-full' resizeMode='cover'/>
     <Text className='text-primary font-bold text-xs'> Get Now </Text>
   </TouchableOpacity>
 </View>
-<View className='absolute inset-0 bg-black/40'></View>
+</View>
     </View>
   ))}
 
